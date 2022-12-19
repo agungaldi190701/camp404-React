5f09f4c42672a850766ad1115582d23c363faff8
agungaldi190701 <agung20.mhs@akb.ac.id>
2022-12-19 11:11:59 +0700

    membuat component dan import Manajemenbuku.jsx

---
+++ src/components/ManajemenBuku.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ManajemenBuku() {
+
+    // data 
+
+    // event handler
+    return (
+        <div className='container-fluid mt-3'>
+            <h1 className='text-center'>Ini Adalah Manajemen Buku</h1>
+            <div id='formTambah'></div>
+            <div id='formUbah'></div>
+            <div id='daftarBuku'></div>
+        </div>
+    )
+}
+
+export default ManajemenBuku; 
