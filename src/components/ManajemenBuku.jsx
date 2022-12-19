2def48b614ab5dc837d37ae745954d6d086e1f86
agungaldi190701 <agung20.mhs@akb.ac.id>
2022-12-19 13:34:57 +0700

    show hide Form di ManajemenBuku.jsx

--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -1,108 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function ManajemenBuku({
-    bookList
-}) {
+function ManajemenBuku({ bookList }) {
 
-    console.log(bookList);
+    // console.log(bookList);
+    const [form, setForm] = useState("");
     // data 
 
     // event handler
+    function ShowFormTambah() {
+        setForm("tambah");
+    }
+    function ShowFormEdit(buku) {
+        setForm("edit");
+    }
+    function HideForm() {
+        setForm("");
+    }
+
     return (
-        <div className='container-fluid mt-3'>
+        <div className='container-fluid mt-3' >
             <h1 className='text-center'>Manajemen Buku</h1>
             <br></br>
             <div id='formTambah'>
-                <div className='card'>
-                    <div className='card-header bg-primary text-white'>
-                        <h5 className='card-title '>Tambah Buku</h5>
+                {form === "tambah" && (
+                    <div className='card'>
+                        <div className='card-header bg-primary text-white'>
+                            <h5 className='card-title '>Tambah Buku</h5>
+                        </div>
+                        <div className='card-body'>
+                            <form>
+                                <div className="row g-3 align-items-center">
+                                    <div className="col-auto">
+
+                                        <input type="text" id="judul" className="form-control" name='judul' placeholder='Judul...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <input type="text" id="pengarang" className="form-control" name='pengarang' placeholder='Pengarang...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <input type="text" id="penerbit" className="form-control" name='penerbit' placeholder='Penerbit...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <input type="text" id="tahun" className="form-control" name='tahun' placeholder='Tahun...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <button type="submit" className="btn btn-primary ">Tambah</button>
+                                    </div>
+
+
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
-                    <div className='card-body'>
-                        <form>
-                            <div className="row g-3 align-items-center">
-                                <div className="col-auto">
-
-                                    <input type="text" id="judul" className="form-control" name='judul' placeholder='Judul...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <input type="text" id="pengarang" className="form-control" name='pengarang' placeholder='Pengarang...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <input type="text" id="penerbit" className="form-control" name='penerbit' placeholder='Penerbit...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <input type="text" id="tahun" className="form-control" name='tahun' placeholder='Tahun...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <button type="submit" className="btn btn-primary ">Tambah</button>
-                                </div>
-
-
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
+                )}
             </div>
             <br></br>
             <div id='formUbah'>
-                <div className='card'>
-                    <div className='card-header bg-dark text-white'>
-                        <h5 className='card-title '>Edit Buku</h5>
+                {form === "edit" && (
+                    <div className='card'>
+                        <div className='card-header bg-dark text-white'>
+                            <h5 className='card-title '>Edit Buku</h5>
+                        </div>
+                        <div className='card-body'>
+                            <form>
+                                <div className="row g-3 align-items-center">
+                                    <div className="col-auto">
+
+                                        <input type="text" id="judul" className="form-control" name='judul' placeholder='Judul...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <input type="text" id="pengarang" className="form-control" name='pengarang' placeholder='Pengarang...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <input type="text" id="penerbit" className="form-control" name='penerbit' placeholder='Penerbit...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <input type="text" id="tahun" className="form-control" name='tahun' placeholder='Tahun...'>
+                                        </input>
+                                    </div>
+                                    <div className="col-auto">
+
+                                        <button type="button" className="btn btn-secondary " onClick={HideForm}>Batal</button>
+                                        <button type="submit" className="btn btn-success mx-2 ">Edit</button>
+                                    </div>
+
+
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
-                    <div className='card-body'>
-                        <form>
-                            <div className="row g-3 align-items-center">
-                                <div className="col-auto">
-
-                                    <input type="text" id="judul" className="form-control" name='judul' placeholder='Judul...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <input type="text" id="pengarang" className="form-control" name='pengarang' placeholder='Pengarang...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <input type="text" id="penerbit" className="form-control" name='penerbit' placeholder='Penerbit...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <input type="text" id="tahun" className="form-control" name='tahun' placeholder='Tahun...'>
-                                    </input>
-                                </div>
-                                <div className="col-auto">
-
-                                    <button type="button" className="btn btn-secondary ">Batal</button>
-                                    <button type="submit" className="btn btn-success mx-2 ">Edit</button>
-                                </div>
-
-
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
+                )}
             </div>
 
-
+            <br></br>
             <div id='daftarBuku'>
                 <div className='card'>
                     <div className='card-header bg-secondary text-white'>
                         <h5 className='card-title'>Daftar Buku</h5>
                     </div>
                     <div className='card-body'>
-                        <button className='btn btn-sm btn-primary mx-2'>Tambah +</button>
+                        <button className='btn btn-sm btn-primary mx-2' onClick={ShowFormTambah}>Tambah +</button>
                         <table className="table table-striped table-hover">
                             <thead>
                                 <tr>
@@ -123,7 +136,7 @@ function ManajemenBuku({
                                         <td>{buku.penerbit}</td>
                                         <td>{buku.tahun}</td>
                                         <td>
-                                            <button className='btn btn-sm btn-warning mx-2'>Ubah</button>
+                                            <button className='btn btn-sm btn-warning mx-2' onClick={ShowFormEdit}>Ubah</button>
                                             <button className='btn btn-sm btn-danger'>Hapus</button>
                                         </td>
                                     </tr>
@@ -135,7 +148,7 @@ function ManajemenBuku({
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
