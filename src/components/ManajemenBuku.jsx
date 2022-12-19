914558f2b0a2db88a33a2e004a49bc5078bff4f5
agungaldi190701 <agung20.mhs@akb.ac.id>
2022-12-19 12:59:15 +0700

    membuat form tambah di ManajemenBuku.jsx

--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -10,13 +10,56 @@ function ManajemenBuku({
     // event handler
     return (
         <div className='container-fluid mt-3'>
-            <h1 className='text-center'>Ini Adalah Manajemen Buku</h1>
-            <div id='formTambah'></div>
+            <h1 className='text-center'>Manajemen Buku</h1>
+            <br></br>
+            <div id='formTambah'>
+                <div className='card'>
+                    <div className='card-header bg-primary text-white'>
+                        <h5 className='card-title '>Tambah Buku</h5>
+                    </div>
+                    <div className='card-body'>
+                        <form>
+                            <div className="row g-3 align-items-center">
+                                <div className="col-auto">
+
+                                    <input type="text" id="judul" className="form-control" name='judul' placeholder='Judul...'>
+                                    </input>
+                                </div>
+                                <div className="col-auto">
+
+                                    <input type="text" id="pengarang" className="form-control" name='pengarang' placeholder='Pengarang...'>
+                                    </input>
+                                </div>
+                                <div className="col-auto">
+
+                                    <input type="text" id="penerbit" className="form-control" name='penerbit' placeholder='Penerbit...'>
+                                    </input>
+                                </div>
+                                <div className="col-auto">
+
+                                    <input type="text" id="tahun" className="form-control" name='tahun' placeholder='Tahun...'>
+                                    </input>
+                                </div>
+                                <div className="col-auto">
+
+                                    <button type="submit" className="btn btn-primary ">Tambah</button>
+                                </div>
+
+
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            <br></br>
             <div id='formUbah'></div>
+
+
             <div id='daftarBuku'>
                 <div className='card'>
-                    <div className='card-header'>
-                        <h3 className='card-title'>Daftar Buku</h3>
+                    <div className='card-header bg-secondary text-white'>
+                        <h5 className='card-title'>Daftar Buku</h5>
                     </div>
                     <div className='card-body'>
                         <button className='btn btn-sm btn-primary mx-2'>Tambah +</button>
